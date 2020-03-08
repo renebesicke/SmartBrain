e1a0ae4bc281f15a3eb307ba672204a97f9d097f
Rene Besicke <renebesicke@gmail.com>
2020-03-08 19:21:49 +0100

    face detection api implemented

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
-import Logo from './components/Logo/Logo'
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
+import Logo from './components/Logo/Logo';
+import Clarifai from 'clarifai';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo />
-      {/*<ImageLinkForm />*/}
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+const app = new Clarifai.App({
+    apiKey: 'cd1fdc72ef3f472cbe041d4cc621ddd8'
+});
+
+const  particlesOptions = {
+    particles: {
+        number: {
+            value: 50,
+            denisty: {
+                enable: true,
+                value_area: 800
+            }
+        }
+    }
+};
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    };
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        console.log('click');
+        app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
+            .then(function(response) {
+                    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+                },
+                function() {
+                    console.log('Error!!');
+                });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Particles className='particles'
+                           params={particlesOptions} />
+                <Navigation/>
+                <Logo />
+                <Rank />
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+                <FaceRecognition imageUrl={this.state.imageUrl}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ smartbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Your current Rank is ...'}
+            </div>
+            <div>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
